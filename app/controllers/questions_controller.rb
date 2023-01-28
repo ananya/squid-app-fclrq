56e9e16afbcffa409fc408a04e86eafddd976d81
ananya <ananyaagrawal125@gmail.com>
2023-01-29 02:40:18 +0530

    add AnswerService

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
 
   def ask
     @question = params[:question]
-    puts @question
+    puts "1", @question
 
     if @question.nil?
       return render json: { error: "No question provided" }, status: 400
@@ -16,8 +16,11 @@ class QuestionsController < ApplicationController
     if !@question.end_with?("?")
       @question = @question + "?"
     end
+    puts "2", @question
     
     @already_asked = Question.find_by(question: @question)
+    puts "3", @already_asked
+
     if @already_asked && @already_asked.answer
       puts "Already asked", @already_asked
       @already_asked.ask_count += 1
@@ -27,11 +30,14 @@ class QuestionsController < ApplicationController
 
     @answer_service = AnswerService.new
 
-    @response = @answer_service.get_answer(question)
+    @response = @answer_service.get_answer(@question)
+    puts "4", @response
+
     if @response[:success]
       @answer = @response[:answer]
       @context = @response[:context]
       @record = Question.create(question: @question, answer: @answer, context: @context)
+      puts "5", @record
       @record.save
       return render json: @record
     end
---
+++ app/services/answer_service.rb
@@ -0,0 +1,126 @@
+class AnswerService
+
+  JSON_FILE = "static.json"  
+  CONTEXT_FILE = Rails.root.join('book.pdf.pages.csv')
+  EMBEDDING_FILE = Rails.root.join('book.pdf.embeddings.csv')
+  COMPLETION_MODEL = "text-davinci-003"
+
+  MAX_SECTION_LEN = 500
+  SEPARATOR = "\n* "
+
+  MODEL_NAME = "curie"
+  DOC_EMBEDDINGS_MODEL = "text-search-#{MODEL_NAME}-doc-001"
+  QUERY_EMBEDDINGS_MODEL = "text-search-#{MODEL_NAME}-query-001"
+
+  def initialize
+    @openai_client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+    @context = self.parse_context_csv
+    @embedding = self.parse_embedding_csv
+  end
+
+  
+  def get_answer(question)
+    prompt, context = self.construct_promt(question)
+    puts "prompt", prompt
+    puts "context", context
+
+    response = @openai_client.completions(
+      parameters: {
+        prompt: prompt,
+        max_tokens: 150,
+        temperature: 0,
+        model: COMPLETION_MODEL,
+      }
+    )
+    puts response
+
+    return {:success => false} if response['choices'].length == 0
+    return {:success => true, :answer => response['choices'][0]['text'], :context => context}
+  end
+
+  private
+
+  def get_relevant_context(question)
+    most_relevant_document_sections = self.order_document_sections_by_query_similarity(question)
+    chosen_sections_len = 0
+    @chosen_sections = []
+
+    most_relevant_document_sections.each do |key, index|
+      document_section = @context[key]
+      chosen_sections_len += document_section[:tokens].to_i + SEPARATOR.size
+      if chosen_sections_len > MAX_SECTION_LEN
+        space_left = MAX_SECTION_LEN - chosen_sections_len - SEPARATOR.size
+        @chosen_sections << SEPARATOR + document_section[:content][...space_left]
+        break
+      end
+      @chosen_sections << SEPARATOR + document_section[:content]
+    end
+    return @chosen_sections
+  end
+
+  def order_document_sections_by_query_similarity(question)
+    query_embedding = self.get_embedding(question)
+    scores = {}
+    @embedding.each do |title, embedding|
+      scores[title] = self.cosine_similarity(query_embedding, embedding)
+    end
+    sorted_scores = scores.sort_by { |title, score| score }.reverse.to_h
+  end
+
+  def cosine_similarity(a, b)
+    """
+    Calculate the cosine similarity between two vectors.
+    """
+    dot_product = a.zip(b).map { |x, y| x * y }.reduce(:+)
+    magnitude_a = Math.sqrt(a.map { |x| x**2 }.reduce(:+))
+    magnitude_b = Math.sqrt(b.map { |x| x**2 }.reduce(:+))
+    return dot_product / (magnitude_a * magnitude_b)
+  end
+
+  def get_embedding(text)
+    result = @openai_client.embeddings(
+      parameters: {
+        model: DOC_EMBEDDINGS_MODEL,
+        input: text
+      } 
+    )
+    return result['data'][0]['embedding']
+  end
+
+  def construct_promt(question)
+    json = JSON.parse(File.read(JSON_FILE))
+    header = json["header"]
+    questions_and_answers = json["questions_and_answers"].join(" ")
+    relevant_context = self.get_relevant_context(question).join(" ")
+    puts "relevant_context", relevant_context.is_a?(String)
+    puts "header", header.is_a?(String)
+    puts "questions_and_answers", questions_and_answers.is_a?(String)
+
+    preamble = header + relevant_context + questions_and_answers + "\n\n\nQ: " + question + "\n\nA: "
+
+    return preamble, relevant_context
+
+  end
+
+  def parse_context_csv
+    context = {}
+    rows = CSV.read(CONTEXT_FILE, headers: true)
+    rows[1..].each_with_index do |row, i|
+      context[row["title"]] = {
+        "content": row["content"],
+        "tokens": row["tokens"],
+      }
+    end
+    return context
+  end
+
+  def parse_embedding_csv
+    embedding = {}
+    rows = CSV.read(EMBEDDING_FILE, headers: true)
+    rows[1..].each_with_index do |row, i|
+      embedding[row[0]] = row[1..].map(&:to_f)
+    end
+    return embedding
+  end
+
+end
